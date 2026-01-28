3fff8e4ed5c670ef96d743298a8d0e291076fed5
Ron <ronshadreck@gmail.com>
2026-01-28 23:09:29 +0200

    Added Job model

---
+++ models/Job.js
@@ -0,0 +1,37 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../database');
+
+const Job = new sequelize.define('Job', {
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UIDV4,
+    primaryKey: true,
+    allowNull: false
+  },
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  description: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  price: {
+    type: DataTypes.INTERGER,
+    allowNull: false
+  },
+  category: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  status: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  location: {
+    type: DataTypes.STRING,
+    allowNull: false
+  }
+});
+
+module.exports = Job;

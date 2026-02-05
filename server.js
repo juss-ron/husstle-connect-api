4af1443539a545caaa6f40168fc77836f8973ea2
Ron <ronshadreck@gmail.com>
2026-02-05 23:22:56 +0200

    added job endpoints

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const sequelize = require('./database');
 const authRoutes = require('./routes/auth');
 const categoryRoutes = require('./routes/category')
+const jobRoutes = require('./routes/jobs')
 const port = 3000
 
 const app = express();
@@ -10,6 +11,7 @@ app.use(express.json());
 
 app.use('/auth', authRoutes);
 app.use('/categories', categoryRoutes);
+app.use('/jobs', jobRoutes);
 
 sequelize.sync().then(() => {
   console.log('Databased synced');
